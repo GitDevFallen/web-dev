0010e80869ef4e674a7969f6ac6deaae98cb19e2
olulic <olulic@foi.hr>
2023-11-14 21:10:47 +0100

    Završen slider, dodani komentari

--- olulic.js
+++ olulic.js
@@ -1,6 +1,14 @@
-/* $(document).ready(function () {
-    $("wrapper").click(function () { 
-        $(this).hide();
-    });
+/*
+$(document).ready(function() {
+    funkcija();
 });
 */
+var prvi = document.getElementsByClassName("slike1");
+var drugi = document.getElementsByClassName("slike2");
+
+var prvaSlika1 = prvi[0];
+var zadnjaSlika1 = prvi[prvi.length-1];
+var prvaSlika2 = drugi[0];
+var zadnjaSlika2 = drugi[drugi.length-1];
+
+var img = $('.wrapper');
